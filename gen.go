f5aefbd3510b9dd0ade95a4217a62a21c796e2b3
Koki Honda <k.honda.kh@future.co.jp>
2021-02-19 10:59:13 +0900

    gen関数をザクっと書いた。全く動いていない

--- gen.go
+++ gen.go
@@ -1,5 +1,60 @@
 package twowaysql
 
-func gen(trees *Tree) (string, []string, error) {
-	return "", nil, nil
+// 抽象構文木から目標文字列を生成
+// 欲しいサブ関数: 空白調整、if,elifの評価、
+// バインド抽出は別のパスにする
+// 左部分木、右部分木と辿る
+// この設計は適切なのだろうか?
+// skipがtrueならappendしない。
+func gen(trees *Tree, skip bool) (string, error) {
+	if trees == nil {
+		return "", nil
+	}
+	res := ""
+	node := trees
+
+	str, err := gen(node.Left, skip)
+	if err != nil {
+		return "", err
+	}
+	switch kind := node.Kind; kind {
+	case NdSQLStmt:
+		res = appendIfNotSkip(res, str, skip)
+	case NdBind:
+		res = appendIfNotSkip(res, bindConvert(str), skip)
+	case NdIf, NdElif:
+		if evalCondition(node.Token.str) {
+			res = appendIfNotSkip(res, str, skip)
+			//ENDが来るまで右部分木をSKIP
+			skip = true
+		}
+	case NdElse:
+	case NdEnd:
+		skip = false
+	case NdEndOfProgram:
+	}
+
+	str, err = gen(node.Right, skip)
+	if err != nil {
+		return "", err
+	}
+
+	return res, nil
+}
+
+func appendIfNotSkip(s1, s2 string, skip bool) string {
+	if !skip {
+		return s1 + s2
+	}
+	return s1
+}
+
+// /*value*/1000 -> ?/*value*/ みたいに変換する
+func bindConvert(str string) string {
+	return str
+}
+
+// /* If ... */ /* Elif ... */の条件を評価する
+func evalCondition(str string) bool {
+	return true
 }
--- gen_test.go
+++ gen_test.go
@@ -4,60 +4,50 @@ import "testing"
 
 func TestGen(t *testing.T) {
 	tests := []struct {
-		name     string
-		input    *Tree
-		want     string
-		wantBind []string
+		name  string
+		input *Tree
+		want  string
 	}{
 		{
-			name:     "",
-			input:    makeEmpty(),
-			want:     "",
-			wantBind: nil,
+			name:  "",
+			input: makeEmpty(),
+			want:  "",
 		},
 		{
-			name:     "no comment",
-			input:    makeNoComment(),
-			want:     "SELECT * FROM person WHERE employee_no < 1000  AND dept_no = 1",
-			wantBind: nil,
+			name:  "no comment",
+			input: makeNoComment(),
+			want:  "SELECT * FROM person WHERE employee_no < 1000  AND dept_no = 1",
 		},
 		{
-			name:     "if",
-			input:    makeTreeIf(),
-			want:     "SELECT * FROM person WHERE employee_no < 1000  AND dept_no = 1",
-			wantBind: nil,
+			name:  "if",
+			input: makeTreeIf(),
+			want:  "SELECT * FROM person WHERE employee_no < 1000  AND dept_no = 1",
 		},
 		{
-			name:     "if and bind",
-			input:    makeTreeIfBind(),
-			want:     "SELECT * FROM person WHERE employee_no < ?/*maxEmpNo*/",
-			wantBind: []string{"maxEmpNo"},
+			name:  "if and bind",
+			input: makeTreeIfBind(),
+			want:  "SELECT * FROM person WHERE employee_no < ?/*maxEmpNo*/",
 		},
 		{
-			name:     "if elif else",
-			input:    makeIfElifElse(),
-			want:     "SELECT * FROM person WHERE employee_no < 1000 AND dept_no =1",
-			wantBind: nil,
+			name:  "if elif else",
+			input: makeIfElifElse(),
+			want:  "SELECT * FROM person WHERE employee_no < 1000 AND dept_no =1",
 		},
 		{
-			name:     "if nest",
-			input:    makeIfNest(),
-			want:     "SELECT * FROM person WHERE employee_no < 1000 /* IF true */  AND id=3",
-			wantBind: nil,
+			name:  "if nest",
+			input: makeIfNest(),
+			want:  "SELECT * FROM person WHERE employee_no < 1000 /* IF true */  AND id=3",
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got, gotBind, err := gen(tt.input); err != nil || got != tt.want || sliceEqual(gotBind, tt.wantBind) {
+			if got, err := gen(tt.input, false); err != nil || got != tt.want {
 				if err != nil {
 					t.Error(err)
 				}
 				if got != tt.want {
 					t.Errorf("Doesn't Match expected: %v, but got: %v\n", tt.want, got)
-				}
-				if !sliceEqual(gotBind, tt.wantBind) {
-					t.Errorf("Bind: Doesn't Match expected: %v, but got: %v\n", tt.wantBind, gotBind)
 				}
 			}
 		})
